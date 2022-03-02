933710fd12fa789339f29adfead8f41839e6318c
Debych <zh.eliseev@yanex.ru>
2022-03-02 22:43:46 +0300

    add limit farmings view function

--- best_apr_backend/best_apr/urls.py
+++ best_apr_backend/best_apr/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ListPoolAprs, ListEternalFarmingsAprs
+from .views import ListPoolAprs, ListEternalFarmingsAprs, ListLimitFarmingsTvl
 
 urlpatterns = [
     path('pools/', ListPoolAprs.as_view(), name='get_pools_apr'),
-    path('eternalFarmings/', ListEternalFarmingsAprs.as_view(), name='get_eternal_farmings_apr')
+    path('eternalFarmings/', ListEternalFarmingsAprs.as_view(), name='get_eternal_farmings_apr'),
+    path('limitFarmings/', ListLimitFarmingsTvl.as_view(), name='get_limit_farmings_tvl'),
 ]
--- best_apr_backend/best_apr/views.py
+++ best_apr_backend/best_apr/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Pool, EternalFarming
+from .models import Pool, EternalFarming, LimitFarming
 from logging import debug
 
 
@@ -31,4 +31,18 @@ class ListEternalFarmingsAprs(APIView):
 
         for farming in EternalFarming.objects.filter(network__title=network_name):
             result[farming.hash] = farming.last_apr
+        return Response(result)
+
+
+class ListLimitFarmingsTvl(APIView):
+    def get(self, request, format=None):
+        result = {}
+
+        try:
+            network_name = request.GET['network']
+        except KeyError:
+            network_name = 'Polygon'
+
+        for farming in LimitFarming.objects.filter(network__title=network_name):
+            result[farming.hash] = farming.matic_amount
         return Response(result)
