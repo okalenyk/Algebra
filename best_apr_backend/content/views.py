52f70f695b845f5cdbb2f3cf8901b24d33f34e62
debych <zh.eliseev@yandex.ru>
2022-05-19 18:22:11 +0300

    refactor to APIView

--- best_apr_backend/content/views.py
+++ best_apr_backend/content/views.py
@@ -1,28 +1,21 @@
 from django.http import JsonResponse
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.views import APIView
 
 from .models import Event, Publication
 from .serializers import EventSerializer, PublicationsSerializer
-from logging import debug
 
 # Create your views here.
 
 
-class ListEvents(ModelViewSet):
-    queryset = Event.objects.all()
-    serializer_class = EventSerializer
-
-    def get(self, request):
-        serializer = self.get_serializer(self.queryset, many=True)
+class ListEvents(APIView):
+    def get(self, request, format=None):
+        serializer = EventSerializer(Event.objects.all(), many=True)
 
         return JsonResponse(serializer.data, safe=False)
 
 
-class ListPublications(ModelViewSet):
-    queryset = Publication.objects.all()
-    serializer_class = PublicationsSerializer
-
-    def get(self, request):
-        serializer = self.get_serializer(self.queryset, many=True)
+class ListPublications(APIView):
+    def get(self, request, format=None):
+        serializer = PublicationsSerializer(Publication.objects.all(), many=True)
 
         return JsonResponse(serializer.data, safe=False)
