923545bae042b09f53f6a868c8a462612fb1e2e7
debych <zh.eliseev@yandex.ru>
2022-06-06 12:28:24 +0300

    add readonly field in admin and sort in api

--- best_apr_backend/content/views.py
+++ best_apr_backend/content/views.py
@@ -9,13 +9,13 @@ from .serializers import EventSerializer, PublicationsSerializer
 
 class ListEvents(APIView):
     def get(self, request, format=None):
-        serializer = EventSerializer(Event.objects.all(), many=True, context={"request": request})
+        serializer = EventSerializer(Event.objects.order_by('-_created_at'), many=True, context={"request": request})
 
         return JsonResponse(serializer.data, safe=False)
 
 
 class ListPublications(APIView):
     def get(self, request, format=None):
-        serializer = PublicationsSerializer(Publication.objects.all(), many=True, context={"request": request})
+        serializer = PublicationsSerializer(Publication.objects.order_by('-_created_at')[:3], many=True, context={"request": request})
 
         return JsonResponse(serializer.data, safe=False)
